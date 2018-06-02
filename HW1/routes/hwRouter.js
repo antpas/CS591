a6495db3921db60fdb2ddd9bd4f516d5d97b9bb2
Anthony Pasquariello <antpas@bu.edu>
2018-06-01 20:54:48 -0400

    Update hwRouter.js

--- HW1/routes/hwRouter.js
+++ HW1/routes/hwRouter.js
@@ -14,7 +14,16 @@ router.get('/:inputParam', function (req, res) {
   })
 
 router.post('/', function (req, res) {
-    res.send(req.body)
+   
+    inputVar = req.body.inputParam
+    inputLength = inputVar.length
+
+    var data = {
+        string: inputVar,
+        length: inputLength
+    };
+
+    res.send(data)
 })
 
 
